e5de485b1e3eb4f0c60f044782be1f7768d556e0
Anton Nazyrov <toctoc4toc@gmail.com>
2021-09-13 00:29:13 +0700

    first commit

---
+++ src/views/TimerView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="timer-view">
+    <div class="timer-view__timer">
+      <Timer />
+    </div>
+  </div>
+</template>
+
+<script>
+import Timer from '@/components/Timer/Timer.vue'
+export default {
+  name: 'Home',
+  components: { Timer },
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/variables.scss';
+
+body {
+  margin: 0;
+}
+
+.timer-view {
+  background: $background;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
